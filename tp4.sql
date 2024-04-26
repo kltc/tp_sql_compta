21eb07faac25eceb6f52a9bbf6436945927578bc
Kevin LE <kevin.letc13@gmail.com>
2024-04-26 11:01:22 +0200

    TP4 fait

---
+++ tp4.sql
@@ -0,0 +1,9 @@
+SELECT * from article;
+SELECT * from article WHERE prix > 2;
+SELECT * from article WHERE prix > 2 AND prix < 6.25;
+SELECT * from article WHERE prix BETWEEN 2 AND 6.25;
+SELECT * from article WHERE ID_FOU = 1 AND prix NOT BETWEEN 2 AND 6.25 ORDER BY prix DESC;
+SELECT * from article WHERE ID_FOU = 1 OR ID_FOU = 3;
+SELECT * from article WHERE ID_FOU IN (1, 3); 
+SELECT * from article WHERE ID_FOU NOT IN (1, 3); 
+SELECT * FROM bon WHERE DATE_CMDE BETWEEN '2019-02-01' AND '2019-04-30';
